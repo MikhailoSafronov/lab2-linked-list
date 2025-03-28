af49c157a533dcb869a51dd7485261521dfffa4f
Сафронов Михайло <mishasafronov1@gmail.com>
2025-03-28 16:42:47 +0200

    Refactoring: Replace circular linked list with built-in Python list

--- linkedlist.py
+++ linkedlist.py
@@ -1,77 +1,22 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-
-class CircularLinkedList:
+class ArrayList:
     def __init__(self):
-        self.head = None
+        self.data = []
 
     def length(self):
-        if not self.head:
-            return 0
-        current = self.head
-        count = 1
-        while current.next != self.head:
-            count += 1
-            current = current.next
-        return count
+        return len(self.data)
 
     def append(self, element):
-        new_node = Node(element)
-        if not self.head:
-            self.head = new_node
-            self.head.next = self.head
-        else:
-            current = self.head
-            while current.next != self.head:
-                current = current.next
-            current.next = new_node
-            new_node.next = self.head
+        self.data.append(element)
 
     def insert(self, element, index):
-        if index < 0 or index > self.length():
+        if index < 0 or index > len(self.data):
             raise IndexError("Invalid index")
-        new_node = Node(element)
-        if index == 0:
-            if not self.head:
-                self.head = new_node
-                self.head.next = self.head
-            else:
-                new_node.next = self.head
-                last = self.head
-                while last.next != self.head:
-                    last = last.next
-                self.head = new_node
-                last.next = self.head
-        else:
-            current = self.head
-            for _ in range(index - 1):
-                current = current.next
-            new_node.next = current.next
-            current.next = new_node
+        self.data.insert(index, element)
 
     def delete(self, index):
-        if index < 0 or index >= self.length():
+        if index < 0 or index >= len(self.data):
             raise IndexError("Invalid index")
-        current = self.head
-        if index == 0:
-            deleted_data = current.data
-            if self.head.next == self.head:
-                self.head = None
-            else:
-                last = self.head
-                while last.next != self.head:
-                    last = last.next
-                self.head = self.head.next
-                last.next = self.head
-            return deleted_data
-        for _ in range(index - 1):
-            current = current.next
-        deleted_data = current.next.data
-        current.next = current.next.next
-        return deleted_data
+        return self.data.pop(index)
 
     def clear(self):
-        self.head = None
+        self.data.clear()
--- tests.py
+++ tests.py
@@ -1,21 +1,21 @@
 import unittest
-from linkedlist import CircularLinkedList
+from linkedlist import ArrayList
 
-class TestCircularLinkedList(unittest.TestCase):
+class TestArrayList(unittest.TestCase):
 
     def test_length(self):
-        cll = CircularLinkedList()
-        self.assertEqual(cll.length(), 0)
-        cll.append('a')
-        self.assertEqual(cll.length(), 1)
+        arr = ArrayList()
+        self.assertEqual(arr.length(), 0)
+        arr.append('a')
+        self.assertEqual(arr.length(), 1)
 
     def test_append_and_delete(self):
-        cll = CircularLinkedList()
-        cll.append('a')
-        cll.append('b')
-        self.assertEqual(cll.delete(0), 'a')
-        self.assertEqual(cll.delete(0), 'b')
-        self.assertEqual(cll.length(), 0)
+        arr = ArrayList()
+        arr.append('a')
+        arr.append('b')
+        self.assertEqual(arr.delete(0), 'a')
+        self.assertEqual(arr.delete(0), 'b')
+        self.assertEqual(arr.length(), 0)
 
 if __name__ == '__main__':
     unittest.main()
